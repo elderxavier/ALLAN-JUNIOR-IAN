c8b04d1d44dff4d77f9faabcd36ce6da54ba3c97
Elder Xavier <eldersxavier@gmail.com>
2015-07-31 19:48:14 -0300

    inserido novas redes sociais; ajustados funções em formulario de email.

--- js/functions.js
+++ js/functions.js
@@ -24,7 +24,12 @@ $(document).ready(function(){
 				'mensagem' : _form.find('#mensagem').val()
 			},
 			success : function(data){
-				_form.find('#resposta').html(data);
+				_form.find('#resposta').html(data);				
+				$('#nome').val('');
+				$('#email').val('');
+				$('#telefone').val('');
+				$('#assunto').val('');
+				$('#mensagem').val('');
 			}
 		})
 		
@@ -103,3 +108,74 @@ function ancora_menu(){
 
 
 
+$(document).ready(function(){     
+        $('#kanji-menu').mouseover(function(event){
+            event.preventDefault(event);
+            //$('.top-logos').show(300);                  
+              $(".top-logos").animate({height: '40px'});
+              $("#topo").animate({margin: '40px 0 0 0'});     
+      });
+      $('#kanji-menu').click(function(event){    
+            event.preventDefault();
+            //$('.top-logos').hide(300);      
+            $(".top-logos").animate({height: '3px'});
+              $("#topo").animate({margin: '1px 0px 0px 0px'});
+      });
+     
+
+  
+      $('.facebook-2').mouseover(function(evt){
+        $('.facebook-2 .social-toltip').css('display','block');            
+        $('.facebook-2').css('opacity','1');
+      });
+      $('.facebook-2').mouseout(function(evt){
+        $('.facebook-2 .social-toltip').css('display','none'); 
+        $('.facebook-2').css('opacity','0.3');   
+        
+      });
+
+      $('.linkedin').mouseover(function(evt){
+        $('.linkedin .social-toltip').css('display','block');
+        $('.linkedin').css('opacity','1');       
+      });
+      $('.linkedin').mouseout(function(evt){
+        $('.linkedin .social-toltip').css('display','none');    
+        $('.linkedin').css('opacity','0.3');       
+      });
+
+      $('.youtube').mouseover(function(evt){
+        $('.youtube .social-toltip').css('display','block'); 
+        $('.youtube').css('opacity','1');          
+      });
+      $('.youtube').mouseout(function(evt){
+        $('.youtube .social-toltip').css('display','none'); 
+        $('.youtube').css('opacity','0.3');   
+      });
+
+      $('.twitter').mouseover(function(evt){
+        $('.twitter .social-toltip').css('display','block'); 
+        $('.twitter').css('opacity','1');   
+      });
+      $('.twitter').mouseout(function(evt){
+        $('.twitter .social-toltip').css('display','none');    
+        $('.twitter').css('opacity','0.3');   
+      });
+
+      $('.skype').mouseover(function(evt){
+        $('.skype .social-toltip').css('display','block'); 
+        $('.skype').css('opacity','1');   
+      });
+      $('.skype').mouseout(function(evt){
+        $('.skype .social-toltip').css('display','none');    
+        $('.skype').css('opacity','0.3');   
+      });
+
+      $('.flickr').mouseover(function(evt){
+        $('.flickr .social-toltip').css('display','block'); 
+        $('.flickr').css('opacity','1');   
+      });
+      $('.flickr').mouseout(function(evt){
+        $('.flickr .social-toltip').css('display','none');    
+        $('.flickr').css('opacity','0.3');   
+      });
+});      
